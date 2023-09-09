1b92b2d06ce36b156ccd4322e5d67460b059fa29
Pablo Calderon <pablocalderoncornejo@gmail.com>
2023-09-09 12:21:07 -0300

    botones Ayuda y Calcular 09-09-2023

--- app/src/main/java/cl/isisur/ejemplo04santos2023/MainActivity.java
+++ app/src/main/java/cl/isisur/ejemplo04santos2023/MainActivity.java
@@ -2,13 +2,30 @@ package cl.isisur.ejemplo04santos2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ayuda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ayuda=(Button) findViewById(R.id.BTAyuda);
+        Intent intentAyuda=new Intent(this, MainActivity3.class);
+        Intent intentCalcular= new Intent(this, MainActivity2.class);
+        ayuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
